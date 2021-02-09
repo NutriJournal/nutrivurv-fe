94221c5cbcd5454287e7364fb26cbf25fef9418c
Dave Kane <davelkane@gmail.com>
2021-02-08 23:41:15 -0600

    Changed all weight log and daily record references to match new queries

--- components/graphs/DoughnutContainer.js
+++ components/graphs/DoughnutContainer.js
@@ -1,67 +1,72 @@
-import { Spacer } from "../Layout/LayoutPrimitives";
-import { useQuery } from "@apollo/react-hooks";
-import { ME, GET_DOUGHNUT_DATA } from "../../gql/queries";
-import DonutGraph from "./DonutGraph";
-import { useRouter } from "next/router";
-import { currentRecords } from "../../lib/utils";
-import { useState, useEffect } from "react";
+import { Spacer } from '../Layout/LayoutPrimitives';
+import { useQuery } from '@apollo/react-hooks';
+import { ME, GET_DOUGHNUT_DATA } from '../../gql/queries';
+import DonutGraph from './DonutGraph';
+import { useRouter } from 'next/router';
+import { currentRecords } from '../../lib/utils';
+import { useState, useEffect } from 'react';
 
 const DonutContainer = () => {
   const { data } = useQuery(GET_DOUGHNUT_DATA);
-  const router = useRouter()
-  const [doughnutData, setDoughnutData] = useState({})
+  const router = useRouter();
+  const [doughnutData, setDoughnutData] = useState({});
   // value useed to add negative bottom margin to the graph if on the food journal page
-  const inJournal = router.asPath.split("/")[1] === "journal"
-  
-  useEffect(() => {
-    data && setDoughnutData(totals)
-  },[data])
+  const inJournal = router.asPath.split('/')[1] === 'journal';
 
-  
+  useEffect(() => {
+    data && setDoughnutData(totals);
+  }, [data]);
+
   const chartData = {
     datasets: [
       {
         data: [doughnutData.fat, doughnutData.carbs, doughnutData.protein],
-        backgroundColor: ["#1F7DA2", "#9ABA19", "#00426C"],
-        hoverBackgroundColor: ["#035c80", "#4D8037", "#002942"],
+        backgroundColor: ['#1F7DA2', '#9ABA19', '#00426C'],
+        hoverBackgroundColor: ['#035c80', '#4D8037', '#002942'],
       },
     ],
-    labels: ["Fat", "Carbs", "Protein"],
+    labels: ['Fat', 'Carbs', 'Protein'],
   };
-  
-  if (!data) return "Loading...";
-  
-  // Default values so the graph isn't busted if it's the first time they've visited for the day
-  const defaultValuesForNoRecords = { fat: .33, carbs: .33, protein: .33 }
-  // Filter for todays records
-  const graphRecords = currentRecords(data.myDailyRecords)
 
-  const totals = !graphRecords.length ? defaultValuesForNoRecords : deriveDoughnutValues(graphRecords)
+  if (!data) return 'Loading...';
+
+  const user = data.user[0];
+  // Default values so the graph isn't busted if it's the first time they've visited for the day
+  const defaultValuesForNoRecords = { fat: 0.33, carbs: 0.33, protein: 0.33 };
+  // Filter for todays records
+  const graphRecords = currentRecords(data.daily_record);
+
+  const totals = !graphRecords.length
+    ? defaultValuesForNoRecords
+    : deriveDoughnutValues(graphRecords);
 
   // Written so that it can be exported to utils if there is use for it somewhere else down tha road
   function deriveDoughnutValues(dataObj) {
-    let newObj = {}
-    Object.keys(dataObj).map(record => {
-      Object.keys(dataObj[record]).map(prop => {        
-        if (typeof dataObj[record][prop] === "number") {
-          newObj[prop] ? 
-          (newObj[prop] = newObj[prop] += dataObj[record][prop]) : 
-          (newObj[prop] = dataObj[record][prop])
-        } 
-      })
-    })
-    return newObj
+    let newObj = {};
+    Object.keys(dataObj).map((record) => {
+      Object.keys(dataObj[record]).map((prop) => {
+        if (typeof dataObj[record][prop] === 'number') {
+          newObj[prop]
+            ? (newObj[prop] = newObj[prop] += dataObj[record][prop])
+            : (newObj[prop] = dataObj[record][prop]);
+        }
+      });
+    });
+    return newObj;
   }
 
-  
   return (
-    <div className={`w-1/5 flex justify-around mt-16 -mr-32 ${ inJournal ? "-mb-24" : ""}`}>
+    <div
+      className={`w-1/5 flex justify-around mt-16 -mr-32 ${
+        inJournal ? '-mb-24' : ''
+      }`}
+    >
       <div className="flex flex-col -mr-12 justify-end text-center pb-6">
         <div className="flex items-baseline">
           <div className="text-3xl">
-            {data.me.profile && data.me.profile.weight
-              ? data.me.profile.weight
-              : "Add Your Weight!"}
+            {user.profile && user.profile.weight
+              ? user.profile.weight
+              : 'Add Your Weight!'}
           </div>
           <div>lbs</div>
         </div>
@@ -72,12 +77,14 @@ const DonutContainer = () => {
         <h2 className="w-full text-center font-semibold text-xl pt-4">
           Daily Intake
         </h2>
-        <p className={`text-center ${!doughnutData.calories ? "hidden" : "" }`}>{`${doughnutData.calories} Calories`}</p>
+        <p
+          className={`text-center ${!doughnutData.calories ? 'hidden' : ''}`}
+        >{`${doughnutData.calories} Calories`}</p>
       </div>
       <div className="flex flex-col -ml-12 justify-end text-center pb-6">
         <div className="flex items-baseline">
           <div className="text-3xl">
-            {data.myWeightLogCount ? data.myWeightLogCount : "Start Tracking!"}
+            {data.weight_log ? data.weight_log.length : 'Start Tracking!'}
           </div>
           <div>days</div>
         </div>
