8ce1ab6cc93ecb51ad665cf01a2e7dee23d6c37c
Dave Kane <davelkane@gmail.com>
2021-02-11 15:21:03 -0600

    Removed unnecessary uri declaration in apollo.js

--- pages/api/getToken.js
+++ pages/api/getToken.js
@@ -3,13 +3,6 @@ import auth0 from '../../lib/Auth0/auth0';
 export default async function session(req, res) {
   try {
     const data = await auth0.getSession(req);
-    // console.log('getToken', data.accessToken);
-    // const accessToken = data.accessToken;
-
-    // const userObj = await Object.values(data)[0];
-    // const token = await Object.values(userObj)[2];
-    // const authObj = await { accessToken: data.accessToken, token: token };
-
     res.send(data);
     res.status(200).end(data);
   } catch (error) {
