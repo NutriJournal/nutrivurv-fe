3b197224027bd49d514d9ee131c05df2c6caa903
Dave Kane <davelkane@gmail.com>
2021-02-12 21:36:49 -0600

    Removed unused code

--- next.config.js
+++ next.config.js
@@ -11,15 +11,12 @@ module.exports = {
     AUTH0_CLIENT_SECRET: process.env.AUTH0_CLIENT_SECRET,
     AUTH0_SCOPE: 'openid profile email',
     AUTH0_AUDIENCE: process.env.AUTH0_AUDIENCE,
-    REDIRECT_URI:
-      process.env.REDIRECT_URI || 'http://localhost:3000/api/callback',
-    POST_LOGOUT_REDIRECT_URI:
-      process.env.POST_LOGOUT_REDIRECT_URI || 'http://localhost:3000/',
+    REDIRECT_URI: process.env.REDIRECT_URI,
+    POST_LOGOUT_REDIRECT_URI: process.env.POST_LOGOUT_REDIRECT_URI,
     SESSION_COOKIE_SECRET: process.env.SESSION_COOKIE_SECRET,
     SESSION_COOKIE_LIFETIME: 7200, // 2 hours
-    GRAPHQL_ENDPOINT:
-      process.env.GRAPHQL_ENDPOINT || 'http://localhost:4000/api',
-    DEPLOYED_URI: process.env.DEPLOYED_URI || 'http://localhost:3000/',
+    GRAPHQL_ENDPOINT: process.env.GRAPHQL_ENDPOINT,
+    DEPLOYED_URI: process.env.DEPLOYED_URI,
     WEBSOCKET_ENDPOINT: process.env.WEBSOCKET_ENDPOINT,
   },
 };
