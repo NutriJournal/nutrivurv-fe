f26f88e2518fe85f0ce3acf10f6d5e0b432ba612
Dave Kane <davelkane@gmail.com>
2021-02-14 12:10:41 -0600

    Serialized response in getToken

--- pages/api/getToken.js
+++ pages/api/getToken.js
@@ -4,8 +4,9 @@ export default async function session(req, res) {
   try {
     const data = await auth0.getSession(req);
     console.log(data);
-    res.send(data);
-    res.status(200).end(data);
+    authCreds = JSON.stringify(data);
+    res.send(authCreds);
+    res.status(200).end(authCreds);
   } catch (error) {
     console.error('in getToken', error);
     res.status(error.status || 500).end(error.message);
