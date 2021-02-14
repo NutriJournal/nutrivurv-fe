7695e40957ac8e238714c56b5cde5ea6d83159f1
Dave Kane <davelkane@gmail.com>
2021-02-14 10:50:48 -0600

    Added second print statement

--- pages/api/getToken.js
+++ pages/api/getToken.js
@@ -3,6 +3,7 @@ import auth0 from '../../lib/Auth0/auth0';
 export default async function session(req, res) {
   try {
     const data = await auth0.getSession(req);
+    console.log(data);
     res.send(data);
     res.status(200).end(data);
   } catch (error) {
