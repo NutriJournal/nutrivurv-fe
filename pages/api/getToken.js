f23848c180820e1c9dd7fd7ef5fd1853c261526e
Dave Kane <davelkane@gmail.com>
2021-02-14 10:04:29 -0600

    Addedx print statement in getToken

--- pages/api/getToken.js
+++ pages/api/getToken.js
@@ -3,11 +3,10 @@ import auth0 from '../../lib/Auth0/auth0';
 export default async function session(req, res) {
   try {
     const data = await auth0.getSession(req);
-    console.log(data);
     res.send(data);
     res.status(200).end(data);
   } catch (error) {
-    console.error(error);
+    console.error('in getToken', error);
     res.status(error.status || 500).end(error.message);
   }
 }
